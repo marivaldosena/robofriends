0a86f953b101f463ea814f9c1ff549ea8afa4581
Marivaldo Sena <msena.ifsp@gmail.com>
2019-01-07 19:23:26 -0200

    Updates

---
+++ precache-manifest.c5273b4df0cd9554551046aee45fd942.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "608058b1386bc3847b8c",
+    "url": "/robofriends/static/css/main.6acf06b0.chunk.css"
+  },
+  {
+    "revision": "608058b1386bc3847b8c",
+    "url": "/robofriends/static/js/main.608058b1.chunk.js"
+  },
+  {
+    "revision": "61bb390ed3b11c3f45f9",
+    "url": "/robofriends/static/css/1.14e430fc.chunk.css"
+  },
+  {
+    "revision": "61bb390ed3b11c3f45f9",
+    "url": "/robofriends/static/js/1.61bb390e.chunk.js"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/sega.12a3db8b.woff"
+  },
+  {
+    "revision": "54df141c7c407f7818397644cddc5e91",
+    "url": "/robofriends/index.html"
+  }
+];
