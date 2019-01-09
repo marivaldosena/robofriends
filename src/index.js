f2d7978b8cb228e7e030d116b5df87ac58801774
Marivaldo Sena <msena.ifsp@gmail.com>
2019-01-09 12:38:55 -0200

    Refatoração de código usando Redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App'
-import './index.css';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, combineReducers } from 'redux'
+import { createLogger } from 'redux-logger'
+import thunkMiddleware from 'redux-thunk'
+import App from './containers/App'
+import './index.css'
+import registerServiceWorker from './registerServiceWorker'
+import { searchRobots, requestRobots } from './reducers'
 import tachyons from 'tachyons'
-
-
 window.tachyons = tachyons
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const logger = createLogger()
+
+const rootReducer = combineReducers({ searchRobots, requestRobots })
+const store = createStore(rootReducer,
+    applyMiddleware(thunkMiddleware, logger))
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root'));
 registerServiceWorker();
